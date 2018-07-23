d618c9da3ace15a8f5ba3260936883ad6a0f16e0
Francisco Sandoval <kiko@franciscos-mbp.lan>
2018-07-22 17:00:25 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,20 @@
+console.log("up and running!");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert('You found a match!');
+	}else{
+		alert('Sorry, try again.')
+	}
+}
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+console.log(cards);
