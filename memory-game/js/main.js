2e641a3ed7b8ca877734dc1f2d64e9cc6770eb75
Francisco Sandoval <kiko@franciscos-mbp.lan>
2018-07-22 18:17:42 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,19 +2,20 @@ console.log("up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert('You found a match!');
-	}else{
-		alert('Sorry, try again.')
-	}
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    } else {
+    console.log("Sorry, try again.");
+    }
 }
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-console.log(cards);
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
